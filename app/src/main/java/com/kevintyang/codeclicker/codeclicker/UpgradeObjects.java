58d56bb9d02523e82b9a67932fbfba912e852c16
youkaithegreat <yangkevint@gmail.com>
2014-07-11 23:35:51 -0500

    Committed Upgrades Class that instantiates all of the objects.

---
+++ app/src/main/java/com/kevintyang/codeclicker/codeclicker/UpgradeObjects.java
@@ -0,0 +1,16 @@
+package com.kevintyang.codeclicker.codeclicker;
+
+/**
+ * Created by Kevin-Local on 7/11/2014.
+ */
+public class UpgradeObjects {
+
+    CodeProducer codeExample = new CodeProducer("ExampleName", .1, "Witty tag line/catch phrase should go here", 1000, 1.1 );
+
+    MoneyProducer moneyExample = new MoneyProducer("ExampleName", .50, "Witty producer tag line", 1000, 1.1);
+
+    CodeClickUpgrades codeClickExample = new CodeClickUpgrades("WittyName", 1, 10000, 2, 1.5);
+
+    MoneyClickUpgrades moneyClickExample = new MoneyClickUpgrades("AwesomeName", 1, 1000000, 2, 1.5);
+          
+}
